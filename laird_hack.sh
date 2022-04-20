0bb5f2f42c7f77e78d421fbb1c57859f56fd42dd
Richard Feliciano <RFeliciano@BeaconEmbedded.com>
2022-04-20 17:01:22 -0500

    Modified setup_beacon.sh script to be compatible with Honister
    The meta-beacon-nxp layer needed to be updated so that the setup
    scripts were compatible with Honister builds
    
    Signed-off-by: Richard Feliciano <RFeliciano@BeaconEmbedded.com>

--- laird_hack.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# This is a hack to copy laird firmware files download from the laird github repository
-# otherwise it has to be copied manually and breaks the build if its missing
-#
-
-CWD=`pwd`
-wget -O $CWD/../release/laird-lwb5-fcc-firmware-7.0.0.326.tar.bz2 "https://github.com/LairdCP/Sterling-LWB-and-LWB5-Release-Packages/releases/download/LRD-REL-7.0.0.326/laird-lwb5-fcc-firmware-7.0.0.326.tar.bz2"
